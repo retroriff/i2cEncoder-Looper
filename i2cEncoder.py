997558abc3298f587b52ca1cd6c2ffd29cd4b40f
Xavi <xcatchot@gmail.com>
2019-08-14 18:07:59 +0300

    Encoders Array

--- i2cEncoder.py
+++ i2cEncoder.py
@@ -10,8 +10,9 @@ from gpiozero import Button
 import i2cEncoderLibV2
 
 
-def init_encoder(min_val, max_val):
+def init_encoder(min_val, max_val, device):
     bus = smbus2.SMBus(1)
+    print(hex(device))
     encoder = i2cEncoderLibV2.i2cEncoderLibV2(bus, 0x21)
 
     encconfig = (i2cEncoderLibV2.INT_DATA |
@@ -34,53 +35,63 @@ int_pin = Button(4)
 
 min_val, max_val = 0, 10.0
 exp_growth = 3
+devices = [[21, 5, 41, 9, 40]]
+encoders = []
 
-encoder = init_encoder(min_val, max_val)
+#for i, line in enumerate(devices[0]):
+  #device_int = int(str(devices[0][i]), 16)
+  #encoders[i] = init_encoder(min_val, max_val, device_int)
+  #print("for")
+  #print(hex(encoders[0][i]))
+
+device_int = int(str(21), 16)
+encoder = init_encoder(min_val, max_val, device_int)
 
 try:
     while True:
-        if int_pin.is_pressed:
-            encoder.updateStatus()
-            change_color = False
-            # Encoder turns right
-            if encoder.readStatus(i2cEncoderLibV2.RINC):
-                change_color = True
+        # Play: if int_pin.is_pressed:
+        encoder.updateStatus()
+        change_color = False
 
-            # Encoder turns left
-            if encoder.readStatus(i2cEncoderLibV2.RDEC):
-                change_color = True
+        # Encoder turns right
+        if encoder.readStatus(i2cEncoderLibV2.RINC):
+            change_color = True
 
-            # Encoder pushed
-            if encoder.readStatus(i2cEncoderLibV2.PUSHP):
-                print("Click")
-                encoder.writeCounter(random.randint(min_val, max_val))
-                change_color = True
+        # Encoder turns left
+        if encoder.readStatus(i2cEncoderLibV2.RDEC):
+            change_color = True
 
-            if change_color:
-                counter = encoder.readCounter32()
-                # Default values
-                c = 0
-                color = '0x00'
-                printColor = "Off"
+        # Encoder pushed
+        if encoder.readStatus(i2cEncoderLibV2.PUSHP):
+            print("Click")
+            encoder.writeCounter(random.randint(min_val, max_val))
+            change_color = True
 
-                # Set color Red
-                if counter > max_val / 2:
-                    color_step = (max_val - counter + 1) ** exp_growth
-                    c = int(255 / color_step)
-                    color = '0x{:02x}{:02x}{:02x}'.format(c, 0, 0)
-                    printColor = "Red"
+        if change_color:
+            counter = encoder.readCounter32()
+            # Default values
+            c = 0
+            color = '0x00'
+            printColor = "Off"
 
-                # Set color Blue
-                if counter < max_val / 2:
-                    colorStep = counter + 1 ** exp_growth
-                    c = int(255 / colorStep)
-                    color = '0x{:02x}{:02x}{:02x}'.format(0, 0, c)
-                    printColor = "Blue"
+            # Set color Red
+            if counter > max_val / 2:
+                color_step = (max_val - counter + 1) ** exp_growth
+                c = int(255 / color_step)
+                color = '0x{:02x}{:02x}{:02x}'.format(c, 0, 0)
+                printColor = "Red"
 
-                if (not encoder.readStatus(i2cEncoderLibV2.RMAX) and
-                        not encoder.readStatus(i2cEncoderLibV2.RMIN)):
-                    encoder.writeRGBCode(int(color, 0))
-                    print('Counter: {} {}: {}'.format(counter, printColor, c))
+            # Set color Blue
+            if counter < max_val / 2:
+                colorStep = counter + 1 ** exp_growth
+                c = int(255 / colorStep)
+                color = '0x{:02x}{:02x}{:02x}'.format(0, 0, c)
+                printColor = "Blue"
+
+            if (not encoder.readStatus(i2cEncoderLibV2.RMAX) and
+                    not encoder.readStatus(i2cEncoderLibV2.RMIN)):
+                encoder.writeRGBCode(int(color, 0))
+                print('Counter: {} {}: {}'.format(counter, printColor, c))
 
 except KeyboardInterrupt:
     encoder.writeInterruptConfig(0xff)
