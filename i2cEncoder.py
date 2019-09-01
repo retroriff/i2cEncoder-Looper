e1f6889b28afb0e7a35c3dda336db585cbed5189
Xavi <xcatchot@gmail.com>
2019-09-01 18:08:05 +0300

    Send OSC Messages

--- i2cEncoder.py
+++ i2cEncoder.py
@@ -5,12 +5,17 @@
 
 import random
 from itertools import cycle
+import argparse
+import os
 
 import smbus2
 from gpiozero import Button
 import i2cEncoderLibV2
+from pythonosc import udp_client
+from pythonosc import osc_message_builder
 
 
+print('Running Samplemàtic on the {}'.format(os.uname()[1].capitalize()))
 
 MIN_VAL, MAX_VAL = 0, 20
 DEVICES = [
@@ -18,6 +23,8 @@ DEVICES = [
     [0x11, 0x20, 0x3],
 ]
 int_pin = Button(4)
+osc_address_patterns = ["/switch", "/amp", "/rate", "/start", "/length"]
+osc_address_patterns_alt = ["/attach", "/pitch"]
 
 def init_encoder(device):
     bus = smbus2.SMBus(1)
@@ -30,7 +37,7 @@ def init_encoder(device):
                  i2cEncoderLibV2.RMOD_X1 |
                  i2cEncoderLibV2.RGB_ENCODER)
     encoder.begin(encconfig)
-    encoder.writeCounter(int(MAX_VAL / 2)) # Debug Python3: preguntar a Marc
+    encoder.writeCounter(int(MAX_VAL / 2))  # Debug Python3: preguntar a Marc
     encoder.writeMax(MAX_VAL)
     encoder.writeMin(MIN_VAL)
     encoder.writeStep(1)
@@ -38,10 +45,29 @@ def init_encoder(device):
 
     return encoder
 
-def change_color(encoder):
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", default="127.0.0.1",
+                    help="The ip of the OSC server")
+parser.add_argument("--port", type=int, default=57120,
+                    help="The port the OSC server is listening on")
+args = parser.parse_args()
+#client = udp_client.SimpleUDPClient(args.ip, args.port)
+client = udp_client.SimpleUDPClient("192.168.8.100", args.port)
+
+
+def send_msg(address, channel, value):
+    msg = osc_message_builder.OscMessageBuilder(address=address)
+    msg.add_arg(channel, arg_type='i')
+    msg.add_arg(value, arg_type='i')
+    msg = msg.build()
+    client.send(msg)
+
+
+def run_encoder(encoder):
     EXP_GROWTH = 3
     counter = encoder.readCounter32()
-    # Default values
+    # Default colors values
     color = '0x00'
     printColor = "Off"
 
@@ -53,42 +79,42 @@ def change_color(encoder):
 
     # Set color Blue
     if counter < MAX_VAL / 2:
-        colorStep = counter + 1 ** EXP_GROWTH 
+        colorStep = counter + 1 ** EXP_GROWTH
         color = '0x0000{:02x}'.format(int(255 / colorStep))
         printColor = "Blue"
 
     if (not encoder.readStatus(i2cEncoderLibV2.RMAX) and
             not encoder.readStatus(i2cEncoderLibV2.RMIN)):
         encoder.writeRGBCode(int(color, 0))
-        print('Counter: {} {}: {}'.format(counter, printColor, color))
+        send_msg(osc_address_patterns[idx], channel, counter)
+        print('{} {} {} ({}, {})'.format(osc_address_patterns[idx], channel, counter, printColor, color))
 
 
 encoders = []
 for idx, channel in enumerate(DEVICES):
     encoders.append([])
     for value in channel:
-        print(value)
         encoders[idx].append(init_encoder(value))
 
 try:
-    for channel, row in enumerate(cycle(encoders)):
-        for encoder in row:
+    for channel, row in cycle(enumerate(encoders)):
+        for idx, encoder in enumerate(row):
             # Play: if int_pin.is_pressed:
             encoder.updateStatus()
 
             # Encoder turns right
             if encoder.readStatus(i2cEncoderLibV2.RINC):
-                change_color(encoder)
+                run_encoder(encoder)
 
             # Encoder turns left
             if encoder.readStatus(i2cEncoderLibV2.RDEC):
-                change_color(encoder)
-            
+                run_encoder(encoder)
+
             # Encoder pushed
             if encoder.readStatus(i2cEncoderLibV2.PUSHP):
                 print("Click")
                 encoder.writeCounter(random.randint(MIN_VAL, MAX_VAL))
-                change_color(encoder)
+                run_encoder(encoder)
 
 
 except KeyboardInterrupt:
