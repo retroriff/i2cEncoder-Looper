17c3a3669e445ff72a342211a4ef8a6e88ccd9a3
Marc Jou <smyslov@gmail.com>
2021-01-05 22:11:40 +0100

    Refactored buttons.py

--- buttons.py
+++ buttons.py
@@ -1,23 +1,17 @@
 # GPIO Button Test
+from itertools import cycle
 from gpiozero import Button
-buttons_gpio  = [13, 19, 26, 11, 5, 6, 9, 10, 17, 22, 27]
-button_gpio   = []
-button_number = 0
-button        = Button( buttons_gpio[button_number] )
-button_status = False
 
-# button_gpio.append( Button( 19 ) )
 
-for idx, value in enumerate(buttons_gpio):
-    print buttons_gpio[idx]
-    # button_gpio.insert( Button( buttons_gpio[button_number] ) )
+def load_buttons():
+    buttons_gpio = [13, 19, 26, 11, 5, 6, 9, 10, 17, 22, 27]
 
-while True:
-    if button.is_pressed:
-        if button_status == False:
-            print "Button " + str(button_number) + " is pressed"
-            button_status = True
-    else:
-        if button_status == True:
-            print "Button " + str(button_number) + " is not pressed"
-            button_status = False
+    return [Button(b_num) for b_num in buttons_gpio]
+
+
+if __name__ == "__main__":
+    buttons = load_buttons()
+
+    for button in cycle(buttons):
+        if button.is_pressed:
+            print(f"Button {button.pin} is pressed")
